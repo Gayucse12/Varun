9540b58e93058291112350aa6349d20c58244623
Gayucse12 <cgayucse12@gmail.com>
2023-12-18 18:08:55 +0530

    add this code

---
+++ src/org/add/Add.java
@@ -0,0 +1,16 @@
+package org.add;
+
+public class Add {
+	 static int  add(int a , int b) {
+		 int sum=a+b;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+     int ans=add(20,10);
+System.out.println(ans);
+	}
+
+	
+
+}
